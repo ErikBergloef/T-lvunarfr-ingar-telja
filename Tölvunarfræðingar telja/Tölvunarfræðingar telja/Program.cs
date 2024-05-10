23eae4779a0dab796c725c15604d509b737e0330
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-05-10 08:58:44 +0200

    Add files via upload

---
+++ Tölvunarfræðingar telja/Tölvunarfræðingar telja/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tölvunarfræðingar
+{
+    class Telja
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+
+            int number = int.Parse(Console.ReadLine());
+
+            int result = number - 1;
+
+            Console.WriteLine(result); 
+
+         
+            
+
+        }
+    }
+}
